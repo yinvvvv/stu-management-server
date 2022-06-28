243fad91919f2c76d888d65af43350b8b9514b5d
Lambozhuang <lambozhuang@icloud.com>
2022-06-28 22:36:28 +0800

    GetCourseList() finished

--- controller/course.go
+++ controller/course.go
@@ -3,8 +3,17 @@ package controller
 import (
 	"fmt"
 	"github.com/gin-gonic/gin"
+	"stu-management-server/util"
 )
 
 func GetCourseList(c *gin.Context) {
 	fmt.Println("getCourseList()")
+	sql := "Select * from zjw_course"
+	rows, _ := util.Db.Query(sql)
+	defer rows.Close()
+	result := util.QueryAndParseRows(rows)
+	c.JSON(200, gin.H{
+		"code": 0,
+		"data": result,
+	})
 }
