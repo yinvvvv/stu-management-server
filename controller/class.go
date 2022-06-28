50b47387685f46020123f5ced55deb56b4676278
Lambozhuang <lambozhuang@icloud.com>
2022-06-28 22:31:24 +0800

    GetCourseByClass() finished

--- controller/class.go
+++ controller/class.go
@@ -8,6 +8,25 @@ import (
 
 func GetCourseByClass(c *gin.Context) {
 	fmt.Println("getCourseByClass()")
+	id := c.Param("id")
+	sql := "select zjw_course.* " +
+		"from zjw_class, zjw_course " +
+		"where zjw_class.id = zjw_course.class_id " +
+		"and zjw_class.id = ?; "
+	stmt, err := util.Db.Prepare(sql)
+	if err != nil {
+		fmt.Println(err)
+	}
+	rows, err2 := stmt.Query(id)
+	if err2 != nil {
+		fmt.Println(err2)
+	}
+	defer rows.Close()
+	result := util.QueryAndParseRows(rows)
+	c.JSON(200, gin.H{
+		"code": 0,
+		"data": result,
+	})
 }
 
 func GetRankByClass(c *gin.Context) {
@@ -23,6 +42,7 @@ func GetClassList(c *gin.Context) {
 		"and zjw_major.id = ?"
 	stmt, _ := util.Db.Prepare(sql)
 	rows, _ := stmt.Query(majorId)
+	defer rows.Close()
 	result := util.QueryAndParseRows(rows)
 	c.JSON(200, gin.H{
 		"code": 0,
