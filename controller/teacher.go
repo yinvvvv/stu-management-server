042f10779438d05c7120e65c2404a84ccf07a701
Lambozhuang <lambozhuang@icloud.com>
2022-06-29 15:12:44 +0800

    GetTeacherCourse() finished

--- controller/teacher.go
+++ controller/teacher.go
@@ -20,4 +20,20 @@ func GetTeacherList(c *gin.Context) {
 
 func GetTeacherCourse(c *gin.Context) {
 	fmt.Println("getTeacherCourse()")
+	id := c.Param("id")
+	sql := `
+		select zjw_course.*
+		from zjw_course, zjw_teacher, zjw_teach
+		where zjw_course.id = zjw_teach.course_id
+		and zjw_teacher.id = zjw_teach.teacher_id
+		and zjw_teacher.id = ?;
+	`
+	stmt, _ := util.Db.Prepare(sql)
+	rows, _ := stmt.Query(id)
+	defer rows.Close()
+	result := util.QueryAndParseRows(rows)
+	c.JSON(200, gin.H{
+		"code": 0,
+		"data": result,
+	})
 }
