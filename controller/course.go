4ca2d267c2fc38271719f1874a8a5ecf1e904d51
Lambozhuang <lambozhuang@icloud.com>
2022-06-29 16:36:46 +0800

    GetCourseRank() finished

--- controller/course.go
+++ controller/course.go
@@ -19,3 +19,24 @@ func GetCourseList(c *gin.Context) {
 		"data": result,
 	})
 }
+
+func GetCourseRank(c *gin.Context) {
+	fmt.Println("getCourseRank()")
+	id := c.Param("id")
+	sql := `
+		select zjw_student.name, zjw_score.score from zjw_score, zjw_course, zjw_student
+		where zjw_score.course_id = zjw_course.id
+		and zjw_score.student_id = zjw_student.id
+		and zjw_course.id = ?
+		order by zjw_score.score desc;
+	`
+	stmt, _ := util.Db.Prepare(sql)
+	defer stmt.Close()
+	rows, _ := stmt.Query(id)
+	defer rows.Close()
+	result := util.QueryAndParseRows(rows)
+	c.JSON(200, gin.H{
+		"code": 0,
+		"data": result,
+	})
+}
